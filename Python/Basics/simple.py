5b54cd26e3fa159fca363482c13c54c2e8d45859
Oneplus <oneplus.lau@gmail.com>
2014-01-13 23:02:12 +0800

    revise python simple

--- Python/Basics/simple.py
+++ Python/Basics/simple.py
@@ -4,18 +4,23 @@
 # This example shows how to use Python to access the LTP API to perform full
 # stack Chinese text analysis including word segmentation, POS tagging, dep-
 # endency parsing, name entity recognization and semantic role labeling and
-# get the result in xml format.
+# get the result in specified format.
 
 import urllib2, urllib
+import sys
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2 or sys.argv[1] not in ["xml", "json", "conll"]:
+        print >> sys.stderr, "usage: %s [xml/json/conll]" % sys.argv[0]
+        sys.exit(1)
+
     uri_base = "http://api.ltp-cloud.com/analysis/?"
     api_key  = "YourApiKey"
     text     = "我爱北京天安门"
     # Note that if your text contain special characters such as linefeed or '&',
     # you need to use urlencode to encode your data
     text     = urllib.quote(text)
-    format   = "xml"
+    format   = sys.argv[1]
     pattern  = "all"
 
     url      = (uri_base 
