029f2c18bfa798920e72d630e1d31b5d81901032
Oneplus <oneplus.lau@gmail.com>
2014-01-13 15:27:21 +0800

    add LTML class and construction LTML

---
+++ Python/Customs/ConstructLTML.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+# This example shows how to construct an LTML class, which is used to transfer
+# custom data for LTP to analyse.
+
+from LTML import LTML
+
+def CustomSegmentation():
+    ltml = LTML()
+    ltml.build_from_words(["天安门", "上", "太阳升"])
+    print ltml.tostring()
+
+def CustomPOSTags():
+    ltml = LTML()
+    ltml.build_from_words([("天安门", "N"),
+                           ("上", "P"),
+                           ("太阳升", "V")])
+    print ltml.tostring()
+
+if __name__=="__main__":
+    CustomSegmentation()
+    CustomPOSTags()
