35a5abb04503675c566d9bafa666c25f268110eb
Oneplus <oneplus.lau@gmail.com>
2014-01-17 01:46:09 +0800

    add custom segment

---
+++ Nodejs/Customs/custom-segment.js
@@ -0,0 +1,57 @@
+var builder     = require('xmlbuilder');
+var querystring = require('querystring');
+var http        = require('http');
+
+var doc = builder.create('xml4nlp');
+var build_from_words = function(words) {
+    var xml4nlp = doc
+        .ele('note')
+            .att('sent', 'y')
+            .att('word', 'y')
+            .att('pos',  'n')
+            .att('dp',   'n')
+            .att('ne',   'n')
+            .att('srl',  'n')
+            .up()
+        .ele('doc')
+            .ele('para')
+                .att('id', '0')
+                .ele('sent')
+                    .att('id', '0')
+
+    for (var i = 0; i < words.length; i ++) {
+        var word = xml4nlp.ele('word');
+        word.att('id', i);
+        word.att('cont', words[i]);
+    }
+
+    return xml4nlp.end();
+}
+
+var data = querystring.stringify({
+    'api_key'   : 'YourApiKey',
+    'text'      : build_from_words(['我', '爱', '北京', '天安门', '。']),
+    'xml_input' : 'true',
+    'pattern'   : 'dp',
+    'format'    : 'xml'
+});
+
+var request = http.request({
+            host   : 'api.ltp-cloud.com',
+            port   : '80',
+            path   : '/analysis/',
+            method : 'post',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': data.length
+            }
+        },
+        function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+            });
+        });
+
+request.write(data);
+request.end();
